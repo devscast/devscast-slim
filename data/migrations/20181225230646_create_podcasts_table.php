ff3e8bee7ff6e0af362670675443bc093e4e86c3
bernard-ng <ngandubernard@gmail.com>
2018-12-26 01:18:19 +0200

    [New] create migrations files and add migration code

---
+++ data/migrations/20181225230646_create_podcasts_table.php
@@ -0,0 +1,50 @@
+<?php
+
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+class CreatePodcastsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->table('podcasts')
+            ->addColumn('name', 'string', ['limit' => 255])
+            ->addColumn('slug', 'string', ['limit' => 300])
+            ->addColumn('description', 'string', ['limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('duration', 'string', ['limit' => 20])
+            ->addColumn('thumb', 'string', ['limit' => 300])
+            ->addColumn('audio', 'string', ['limit' => 350])
+            ->addColumn('categories_id', 'int')
+            ->addColumn('users_id', 'int')
+            ->addForeignKey('category_id', 'categories')
+            ->addForeignKey('users_id', 'users')
+            ->addColumn('created_at', 'date')
+            ->create();
+    }
+}
