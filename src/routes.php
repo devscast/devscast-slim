60e5aaff9b2186dee4c541b6f5d9f88a2ca3fecd
bernard-ng <ngandubernard@gmail.com>
2018-12-26 01:58:06 +0200

    [Update] enable cors routing

--- src/routes.php
+++ src/routes.php
@@ -2,4 +2,10 @@
 
 use App\Resources\HomeResource;
 
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler;
+    return $handler($req, $res);
+});
+
 $app->get('/', [HomeResource::class, 'index'])->setName('home');
+
