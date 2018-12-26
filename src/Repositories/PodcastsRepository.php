6d29122b14c8517063c291782194dc247a7236e6
bernard-ng <ngandubernard@gmail.com>
2018-12-26 03:09:51 +0200

    [Update] override the all() method

--- src/Repositories/PodcastsRepository.php
+++ src/Repositories/PodcastsRepository.php
@@ -22,4 +22,23 @@ class PodcastsRepository extends Repository
      * @var PodcastsEntity
      */
     protected $entity = PodcastsEntity::class;
+
+
+    /**
+     * @inheritdoc
+     * @return mixed
+     */
+    public function all()
+    {
+        $sql = <<< SQL
+SELECT {$this->getTable()}.* , categories.name AS category, users.name AS username
+FROM {$this->getTable()}
+LEFT JOIN categories ON {$this->getTable()}.categories_id = categories.id
+LEFT JOIN users ON {$this->getTable()}.users_id = users.id
+ORDER BY {$this->getTable()}.id DESC
+LIMIT 6 OFFSET 0
+SQL;
+
+        return $this->query($sql);
+    }
 }
