5b45fe57ff49bf51cb2cef5c418500b4fe454c4c
bernard-ng <ngandubernard@gmail.com>
2018-12-26 13:53:19 +0200

    [Update] add the latest() method with limit required

--- src/Repositories/PodcastsRepository.php
+++ src/Repositories/PodcastsRepository.php
@@ -36,9 +36,27 @@ FROM {$this->getTable()}
 LEFT JOIN categories ON {$this->getTable()}.categories_id = categories.id
 LEFT JOIN users ON {$this->getTable()}.users_id = users.id
 ORDER BY {$this->getTable()}.id DESC
-LIMIT 6 OFFSET 0
 SQL;
 
+        return $this->query($sql);
+    }
+
+
+    /**
+     * Get the latest podcast
+     * @param int $limit
+     * @return mixed
+     */
+    public function latest(int $limit)
+    {
+        $sql = <<< SQL
+SELECT {$this->getTable()}.* , categories.name AS category, users.name AS username
+FROM {$this->getTable()}
+LEFT JOIN categories ON {$this->getTable()}.categories_id = categories.id
+LEFT JOIN users ON {$this->getTable()}.users_id = users.id
+ORDER BY {$this->getTable()}.id DESC
+LIMIT {$limit} OFFSET 0
+SQL;
         return $this->query($sql);
     }
 }
