b4f7eef0572f400c0d7e5e6a9e4dd7e7846f1612
bernard-ng <ngandubernard@gmail.com>
2018-12-26 13:52:35 +0200

    [Fix] remove map method and add the podcasts routes

--- src/routes.php
+++ src/routes.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Resources\HomeResource;
+use App\Resources\PodcastsResource;
 
-$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+/*$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
     $handler = $this->notFoundHandler;
     return $handler($req, $res);
-});
+});*/
 
-$app->get('/', [HomeResource::class, 'index'])->setName('home');
 
+$app->get('/home', [HomeResource::class, 'index'])->setName('home');
+$app->get('/podcasts', [PodcastsResource::class, 'index'])->setName('podcasts.index');
+$app->get('/podcasts/{slug}-{id}', [PodcastsResource::class, 'show'])->setName('podcasts.show');
