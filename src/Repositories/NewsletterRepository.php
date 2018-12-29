0d91877b9d0134a385a09148b84980bda7f1cace
bernard-ng <ngandubernard@gmail.com>
2018-12-29 20:11:54 +0200

    [Update] add logic in newsletter entity and repository

--- src/Repositories/NewsletterRepository.php
+++ src/Repositories/NewsletterRepository.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Repositories;
+use App\Entities\NewsletterEntity;
 
 /**
  * Class NewsletterRepository
@@ -7,5 +8,18 @@ namespace App\Repositories;
  */
 class NewsletterRepository extends Repository
 {
+
+    /**
+     * the name of the table in the database
+     * @var string
+     */
+    protected $table = 'newsletter';
+
+
+    /**
+     * the class that represent one newsLetter instance
+     * @var NewsletterEntity
+     */
+    protected $entity = NewsletterEntity::class;
 
 }
