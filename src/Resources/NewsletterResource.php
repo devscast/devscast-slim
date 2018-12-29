832d623b2a509bb091f7d7298d1499f1e7134c77
bernard-ng <ngandubernard@gmail.com>
2018-12-29 22:30:50 +0200

    [Update] add validation in the newletter store process

--- src/Resources/NewsletterResource.php
+++ src/Resources/NewsletterResource.php
@@ -3,6 +3,8 @@ namespace App\Resources;
 
 
 use App\Repositories\NewsletterRepository;
+use Awurth\SlimValidation\Validator;
+use Respect\Validation\Validator as v;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -27,18 +29,31 @@ class NewsletterResource
     public function __construct(ContainerInterface $container)
     {
         $this->newsletter = $container->get(NewsletterRepository::class);
+        $this->validator = $container->get(Validator::class);
     }
 
 
     /**
+     * save a email to the newsletter
      * @param ServerRequestInterface $request
      * @param ResponseInterface|Response $response
      * @return ResponseInterface|Response
      */
     public function store(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
-        $email = strval($request->getParsedBody()['email']);
+        $this->validator->validate($request, [
+            'email' => v::email()
+        ]);
 
-
+        if ($this->validator->isValid()) {
+            $email = strval($request->getParsedBody()['email']);
+            $this->newsletter->create(compact('email'));
+            return $response->withJson([
+                'api.message' => 'registration to the newsletter',
+                'api.flash' => 'success'
+            ]);
+        } else {
+            return $response->withJson($this->validator->getErrors())->withJson(400);
+        }
     }
 }
