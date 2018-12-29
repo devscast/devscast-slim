a917363eb954240608af0d8179311550feae3960
bernard-ng <ngandubernard@gmail.com>
2018-12-29 23:36:11 +0200

    [Fix] use correctly validator and send a 422 http response code when data are invalid

--- src/Resources/NewsletterResource.php
+++ src/Resources/NewsletterResource.php
@@ -2,6 +2,7 @@
 namespace App\Resources;
 
 
+use App\Entities\NewsletterEntity;
 use App\Repositories\NewsletterRepository;
 use Awurth\SlimValidation\Validator;
 use Respect\Validation\Validator as v;
@@ -34,16 +35,14 @@ class NewsletterResource
 
 
     /**
-     * save a email to the newsletter
+     * Check validity of and emaill and save it into the newsletter table
      * @param ServerRequestInterface $request
      * @param ResponseInterface|Response $response
      * @return ResponseInterface|Response
      */
     public function store(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
-        $this->validator->validate($request, [
-            'email' => v::email()
-        ]);
+        $this->validator->validate($request, NewsletterEntity::getValidationRules());
 
         if ($this->validator->isValid()) {
             $email = strval($request->getParsedBody()['email']);
@@ -53,7 +52,11 @@ class NewsletterResource
                 'api.flash' => 'success'
             ]);
         } else {
-            return $response->withJson($this->validator->getErrors())->withJson(400);
+            return $response->withJson([
+                'api.validation.errors' => [
+                    'email' => $this->validator->getErrors('email')
+                ]
+            ])->withStatus(422);
         }
     }
 }
