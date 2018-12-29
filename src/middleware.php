fca4a79f2d45b5abf006182ee7c6d5f5aacd81e2
bernard-ng <ngandubernard@gmail.com>
2018-12-29 17:36:24 +0200

    [Update] add a middleware to handle only Xhr requests

--- src/middleware.php
+++ src/middleware.php
@@ -1,14 +1,35 @@
 <?php
 // Application middleware
 
-$app->options('/{routes:.+}', function ($request, $response, $args) {
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+$app->options('/{routes:.+}', function (Request $request, Response $response, array $args) {
     return $response;
 });
 
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
+
+/**
+ * Whether the request is sent with XHR header
+ */
+$app->add(function (Request $request, Response $response, $next) {
+   if ($request->isXhr()) {
+       return $next($request, $response);
+   } else {
+       return $response->withJson(['api.message' => 'Access Forbidden'])->withStatus(403);
+   }
+});
+
+
+/**
+ * Enable CORS for the frontend app
+ */
+$app->add(function (Request $request, Response $response, $next) {
+    $response = $next($request, $response);
     return $response
         ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+        ->withHeader('X-Powered-By', 'Devcast Team');
 });
