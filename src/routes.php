a901a7cd3189431c0cbca8582aa87a733813e821
bernard-ng <ngandubernard@gmail.com>
2018-12-29 17:05:59 +0200

    [Update] add categories Resource routes

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Resources\CategoriesResource;
 use App\Resources\HomeResource;
 use App\Resources\PodcastsResource;
 
@@ -12,9 +13,16 @@ use App\Resources\PodcastsResource;
 $app->get('/home', [HomeResource::class, 'index'])->setName('home');
 
 
-/*
+/**
  * PODCATS RESOURCE ROUTES
  */
 $app->get('/podcasts', [PodcastsResource::class, 'index'])->setName('podcasts.index');
 $app->get('/podcasts/last', [PodcastsResource::class, 'last'])->setName('podcasts.last');
 $app->get('/podcasts/{slug}-{id}', [PodcastsResource::class, 'show'])->setName('podcasts.show');
+
+
+/**
+ * CATEGORIES RESOURCE ROUTES
+ */
+$app->get('/categories', [CategoriesResource::class, 'index'])->setName('categories.index');
+$app->get('/categories/{id:[0-9]+}', [CategoriesResource::class, 'show'])->setName('categories.show');
