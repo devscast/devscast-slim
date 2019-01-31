c912eef9b0e963bd1d772239aad528701ef4fa1d
bernard-ng <ngandubernard@gmail.com>
2019-01-31 23:14:00 +0200

    [Update] add crud action routes for admin module

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,10 @@
 <?php
 
+use Admin\Controllers\CategoriesController;
 use Admin\Controllers\DashboardController;
+use Admin\Controllers\GalleryController;
+use Admin\Controllers\PodcastsController;
+use Admin\Controllers\UsersController;
 use App\Resources\CategoriesResource;
 use App\Resources\HomeResource;
 use App\Resources\NewsletterResource;
@@ -45,6 +49,58 @@ $app->group('/categories', function () {
  */
 $app->group('/admin', function () {
     $this->get('', [DashboardController::class, 'index'])->setName('admin.index');
+
+    $this->group('/podcasts', function () {
+        $this->get('', [PodcastsController::class, 'index'])->setName('admin.podcasts');
+        $this->get('/create', [PodcastsController::class, 'create'])->setName('admin.podcasts.create');
+        $this->post('/store', [PodcastsController::class], 'store')->setName('admin.podcasts.store');
+        $this->get('/{id:[0-9}', [PodcastsController::class, 'edit'])->setName('admin.podcasts.edit');
+        $this->put('/{id:[0-9}', [PodcastsController::class, 'update'])->setName('admin.podcasts.update');
+        $this->delete('/{id:[0-9}', [PodcastsController::class, 'delete'])->setName('admin.podcasts.delete');
+    });
+
+    $this->group('/categories', function () {
+        $this->get('', [CategoriesController::class, 'index'])->setName('admin.categories');
+        $this->get('/create', [CategoriesController::class, 'create'])->setName('admin.categories.create');
+        $this->post('/store', [CategoriesController::class, 'store'])->setName('admin.categories.store');
+        $this->get('/{id:[0-9}', [CategoriesController::class, 'edit'])->setName('admin.categories.edit');
+        $this->put('/{id:[0-9}', [CategoriesController::class, 'update'])->setName('admin.categories.update');
+        $this->delete('/{id:[0-9}', [CategoriesController::class, 'delete'])->setName('admin.categories.delete');
+    });
+
+    $this->group('/gallery', function () {
+        $this->get('', [GalleryController::class, 'index'])->setName('admin.gallery');
+        $this->get('/create', [GalleryController::class, 'create'])->setName('admin.gallery.create');
+        $this->post('/store', [GalleryController::class, 'store'])->setName('admin.gallery.store');
+        $this->get('/{id:[0-9}', [GalleryController::class, 'edit'])->setName('admin.gallery.edit');
+        $this->put('/{id:[0-9}', [GalleryController::class, 'update'])->setName('admin.gallery.update');
+        $this->delete('/{id:[0-9}', [GalleryController::class, 'delete'])->setName('admin.gallery.delete');
+    });
+
+    $this->group('/podcast-links', function () {
+        $this->get('', [])->setName('admin.podcastLinks');
+        $this->get('/create', [])->setName('admin.podcastLinks.create');
+        $this->post('/store', [])->setName('admin.podcastLinks.store');
+        $this->get('/{id:[0-9}', [])->setName('admin.podcastLinks.edit');
+        $this->put('/{id:[0-9}', [])->setName('admin.podcastLinks.update');
+        $this->delete('/{id:[0-9}', [])->setName('admin.podcastsLinks.delete');
+    });
+
+    $this->group('/users', function () {
+        $this->get('', [UsersController::class, 'index'])->setName('admin.users');
+        $this->get('/create', [UsersController::class, 'create'])->setName('admin.users.create');
+        $this->post('/store', [UsersController::class, 'store'])->setName('admin.users.store');
+        $this->get('/{id:[0-9}', [UsersController::class, 'edit'])->setName('admin.users.edit');
+        $this->put('/{id:[0-9}', [UsersController::class, 'update'])->setName('admin.users.update');
+        $this->delete('/{id:[0-9}', [UsersController::class, 'delete'])->setName('admin.users.delete');
+    });
+
+    $this->group('/newsletter', function () {
+        $this->get('', [])->setName('admin.newsletter');
+        $this->get('/create', [])->setName('admin.newsletter.create');
+        $this->post('/send', [])->setName('admin.newsletter.send');
+        $this->delete('/{id:[0-9}', [])->setName('admin.newsletter.delete');
+    });
 });
 
 
