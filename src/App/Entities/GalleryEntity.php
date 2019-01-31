382b16130c953bfd6d51c43c982d2032edc2a3a8
bernard-ng <ngandubernard@gmail.com>
2019-01-31 21:35:08 +0200

    [New] create missing entity class since last migration

---
+++ src/App/Entities/GalleryEntity.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Entities;
+
+/**
+ * Class GalleryEntity
+ * @package App\Entities
+ */
+class GalleryEntity
+{
+
+}
---
+++ src/App/Entities/PodcastLinksEntity.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Entities;
+
+/**
+ * Class PodcastLinksEntity
+ * @package App\Entities
+ */
+class PodcastLinksEntity
+{
+
+}
