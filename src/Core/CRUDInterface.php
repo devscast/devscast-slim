f864e8e4704efbf2923223155d6d4be1058c0afb
bernard-ng <ngandubernard@gmail.com>
2019-01-31 23:12:40 +0200

    [New] create CRUDInterface for crud action in admin module

---
+++ src/Core/CRUDInterface.php
@@ -0,0 +1,64 @@
+<?php
+namespace Core;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+/**
+ * describe how a crud controller should work
+ * Interface CRUDControllerInterface
+ * @package Admin
+ */
+interface CRUDInterface
+{
+
+    /**
+     * @param ServerRequestInterface|Request $request
+     * @param ResponseInterface|Response $response
+     * @return ResponseInterface
+     */
+    public function index(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface;
+
+
+    /**
+     * @param ServerRequestInterface|Request $request
+     * @param ResponseInterface|Response $response
+     * @return ResponseInterface
+     */
+    public function create(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface;
+
+
+    /**
+     * @param ServerRequestInterface|Request $request
+     * @param ResponseInterface|Response $response
+     * @return ResponseInterface
+     */
+    public function store(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface;
+
+
+    /**
+     * @param ServerRequestInterface|Request $request
+     * @param ResponseInterface|Response $response
+     * @return ResponseInterface
+     */
+    public function edit(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface;
+
+
+    /**
+     * @param ServerRequestInterface|Request $request
+     * @param ResponseInterface|Response $response
+     * @return ResponseInterface
+     */
+    public function update(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface;
+
+
+    /**
+     * @param ServerRequestInterface|Request $request
+     * @param ResponseInterface|Response $response
+     * @return ResponseInterface
+     */
+    public function delete(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface;
+
+}
