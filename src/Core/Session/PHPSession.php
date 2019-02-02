f16e5d3905874db7e4d48d3a7f0d235ba71746ad
bernard-ng <ngandubernard@gmail.com>
2019-02-02 16:15:20 +0200

    [Update] implement the sessionInterface and create flashService

---
+++ src/Core/Session/PHPSession.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This file is part of the devcast.
+ *
+ * (c) Bernard Ng <ngandubernard@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core\Session;
+
+/**
+ * Class PHPSession
+ * @package Core\Session
+ */
+class PHPSession implements SessionInterface
+{
+
+    /**
+     * whether the session is active
+     */
+    private function ensureStarted(): void
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_name("ngpictures_ssid");
+            session_start();
+        }
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public function get(string $key, $default = null)
+    {
+        $this->ensureStarted();
+        if (array_key_exists($key, $_SESSION)) {
+            return $_SESSION[$key];
+        }
+        return $default;
+    }
+
+    /**
+     * @param string $key
+     * @param $value
+     * @return void
+     */
+    public function set(string $key, $value): void
+    {
+        $this->ensureStarted();
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * @param string $key
+     * @return void
+     */
+    public function delete(string $key): void
+    {
+        $this->ensureStarted();
+        if (array_key_exists($key, $_SESSION)) {
+            unset($_SESSION[$key]);
+        }
+    }
+}
