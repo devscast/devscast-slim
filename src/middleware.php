1682f68ca9ee78fcb541ddbc0d9eb8f300f117f4
bernard-ng <ngandubernard@gmail.com>
2019-02-02 11:16:47 +0200

    [Update] the middleware pipeline, remove callbacks, use classes

--- src/middleware.php
+++ src/middleware.php
@@ -18,28 +18,5 @@ $app->options('/{routes:.+}', function (Request $request, Response $response, ar
     return $response;
 });
 
-
-/**
- * Check if the request is a json one
- */
-$app->add(function (Request $request, Response $response, $next) {
-    if ($request->getHeader('CONTENT_TYPE') != 'application/json') {
-        if ($request->hasHeader('authorization')) {
-            $request->withAttribute('IsJson', true);
-        }
-    }
-    return $next($request, $response);
-});
-
-
-/**
- * Enable CORS for the frontend app
- */
-$app->add(function (Request $request, Response $response, $next) {
-    $response = $next($request, $response);
-    return $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-        ->withHeader('X-Powered-By', 'Devcast Team');
-});
+$app->add(\App\Middlewares\JsonRequestMiddleware::class);
+$app->add(\App\Middlewares\EnableCORSMiddleware::class);
