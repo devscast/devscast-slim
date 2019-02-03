47519734672ebf4c4e5bab12287917c52ad029f4
bernard-ng <ngandubernard@gmail.com>
2019-02-03 02:54:41 +0200

    [Update] add update validations rules

--- src/App/Entities/PodcastsEntity.php
+++ src/App/Entities/PodcastsEntity.php
@@ -10,11 +10,61 @@
 
 namespace App\Entities;
 
+use Respect\Validation\Validator as v;
+
 /**
  * Class PodcastsEntity
  * @package App\Entities
  */
 class PodcastsEntity
 {
+    /**
+     * validation rules
+     * @var array
+     */
+    private static $validationRules = [];
 
+    /**
+     * validation rules when there is an update
+     * @var array
+     */
+    private static $updateValidationRules = [];
+    
+    /**
+     * retrieve validation rules for an entity
+     * @return array
+     */
+    public static function getValidationRules(): array
+    {
+        if (empty(self::$validationRules)) {
+            self::$validationRules = [
+                'name' => v::notEmpty()->setName('Name'),
+                'slug' => v::optional(v::alnum())->setName('Slug'),
+                'description' => v::notEmpty()->notBlank()->setName('Description'),
+                'duration' => v::notEmpty()->alnum(':')->setName('Durantion'),
+                'thumb' => v::notEmpty()->setName('Thumb Cover'),
+                'audio' => v::notEmpty()->setName('Audio'),
+                'categories_id' => v::numeric()->setName('Categories Id'),
+                'users_id' => v::numeric()->setName('Users Id')
+            ];
+        }
+        return self::$validationRules;
+    }
+
+    /**
+     * retrieve update validation rules for an entity
+     * @return array
+     */
+    public static function getUpdateValidationRules(): array
+    {
+        if(empty(self::$updateValidationRules)) {
+            self::$updateValidationRules = [
+                'name' => v::notEmpty()->setName('Name'),
+                'slug' => v::optional(v::alnum())->setName('Slug'),
+                'description' => v::optional(v::notEmpty()->notBlank())->setName('Description'),
+                'categories_id' => v::optional(v::numeric())->setName('Categories Id'),
+            ];
+        }
+        return self::$updateValidationRules;
+    }
 }
