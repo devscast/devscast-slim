11532c8650b42d7d64c36adcbf0e9f02294cd62d
bernard-ng <ngandubernard@gmail.com>
2019-02-08 20:23:11 +0200

    [Feature] create QueryBuilder

---
+++ src/Core/Database/Builder/Literal.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Core\Database\Builder;
+
+/**
+ * SQL literal value
+ */
+class Literal
+{
+
+    /** @var string */
+    protected $value = '';
+
+    /**
+     * Create literal value
+     *
+     * @param string $value
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Get literal value
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->value;
+    }
+}
