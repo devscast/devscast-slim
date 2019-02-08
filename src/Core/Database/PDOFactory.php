6cbf7d2dd36214fb6ac373cb51081efd5b7d699b
bernard-ng <ngandubernard@gmail.com>
2019-02-08 20:16:28 +0200

    [New] create a PDOFactory for using a queryBuilder

---
+++ src/Core/Database/PDOFactory.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This file is part of the devcast.
+ *
+ * (c) Bernard Ng <ngandubernard@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core\Database;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * Class PDOFactory
+ * @package Core\Database
+ */
+class PDOFactory
+{
+
+    /**
+     * @var null|\PDO
+     */
+    private $PDO = null;
+
+
+    /**
+     * @param ContainerInterface $container
+     * @return \PDO
+     * @internal param string $host
+     * @internal param string $dbname
+     * @internal param string $username
+     * @internal param string $password
+     */
+    public function __invoke(ContainerInterface $container): \PDO
+    {
+        if (is_null($this->PDO)) {
+            $host = $container->get('database.host');
+            $dbname = $container->get('database.name');
+            $username = $container->get('database.username');
+            $password = $container->get('database.password');
+
+            try {
+                $attribute = [
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
+                    \PDO::ATTR_ORACLE_NULLS => \PDO::NULL_EMPTY_STRING,
+                ];
+
+                $PDO = new \PDO("mysql:Host={$host};dbname={$dbname};charset=utf8", $username, $password, $attribute);
+                $this->PDO = $PDO;
+                return $this->PDO;
+            } catch (\PDOException|\Exception $e) {
+                throw new \PDOException($e->getMessage());
+            }
+        }
+        return $this->PDO;
+    }
+}
