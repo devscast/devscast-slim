bd0709950b4d6f385115552d480217a88e56de79
bernard-ng <ngandubernard@gmail.com>
2019-03-06 23:02:55 +0200

    [New] create a twig  extesion for auth module

---
+++ src/Core/Twig/AuthTwigExtension.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of the devcast.
+ *
+ * (c) Bernard Ng <ngandubernard@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Core\Twig;
+
+use Core\Auth\AuthInterface;
+
+
+/**
+ * Class AuthTwigExtension
+ * @package Core\Twig
+ */
+class AuthTwigExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
+{
+
+    /**
+     * @var AuthInterface
+     */
+    private $auth;
+
+    public function __construct(AuthInterface $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Returns a list of global variables to add to the existing list.
+     *
+     * @return array An array of global variables
+     */
+    public function getGlobals(): array
+    {
+        return [
+            'currentUser' => $this->auth->getUser()
+        ];
+    }
+}
