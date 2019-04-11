42d19db190b99117ba4d00708926fdeaa7ab18c3
bernard-ng <ngandubernard@gmail.com>
2019-04-11 18:03:50 +0200

    [New] create QuoteExtension

---
+++ src/App/Twig/QuoteTwigExtension.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of the devcast.
+ *
+ * (c) Bernard Ng <ngandubernard@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Twig;
+
+use App\Repositories\QuotesRepository;
+
+/**
+ * Class QuoteTwigExtension
+ * @package App\Twig
+ */
+class QuoteTwigExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
+{
+    /**
+     * @var QuotesRepository
+     */
+    private $repository;
+
+    /**
+     * QuoteTwigExtension constructor.
+     * @param QuotesRepository $repository
+     */
+    public function  __construct(QuotesRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Returns a list of global variables to add to the existing list.
+     *
+     * @return array An array of global variables
+     */
+    public function getGlobals()
+    {
+        return [
+            'quote' => $this->repository->random()
+        ];
+    }
+}
