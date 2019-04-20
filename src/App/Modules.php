4fe072088a1aa4b0d07e3a8c2caef4a6538efd1f
bernard-ng <ngandubernard@gmail.com>
2019-04-20 21:33:17 +0200

    [New] create module list to avoid code repetition

---
+++ src/App/Modules.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the devcast.
+ *
+ * (c) Bernard Ng <ngandubernard@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App;
+
+/**
+ * list and name of application's modules
+ * Class Modules
+ * @package App
+ * @author bernard-ng, https://bernard-ng.github.io
+ */
+abstract class Modules
+{
+    public const GALLERY = "gallery";
+    public const USERS = "users";
+    public const NEWSLETTER = "newsletter";
+    public const PODCASTLINKS = "podcastLinks";
+    public const PODCASTS = "podcasts";
+    public const CATEGORIES = "categories";
+}
