0b557bd9bec593f224f84d929f4e8118efadc94b
bernard-ng <ngandubernard@gmail.com>
2019-07-19 02:01:58 +0200

    [Add] add swagger and starting respect OpenApi v3

---
+++ swagger/swagger.php
@@ -0,0 +1,12 @@
+<?php
+require('../vendor/autoload.php');
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(title="API Devscast", version="0.1")
+ * @OA\Server(
+ *  url="http://devs-cast.com/api/v1/",
+ *  description="Server API"
+ * )
+ */
